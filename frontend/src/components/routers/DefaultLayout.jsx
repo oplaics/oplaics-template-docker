e0dfd31d67d52219a35f5f6de051ca12620aa13f
Recker <joseortiz112001@gmail.com>
2026-04-10 11:23:48 -0400

    feat: envolver DefaultLayout con el proveedor de Redux

--- frontend/src/components/routers/DefaultLayout.jsx
+++ frontend/src/components/routers/DefaultLayout.jsx
@@ -3,10 +3,20 @@
  */
 import { Outlet } from "react-router-dom";
 
+/**
+ * Redux
+ */
+import { Provider } from "react-redux";
+import store from "../../store";
+
 function DefaultLayoutWrapper() {
   return <Outlet />;
 }
 
 export default function DefaultLayout() {
-  return <DefaultLayoutWrapper />;
+  return (
+    <Provider store={store}>
+      <DefaultLayoutWrapper />
+    </Provider>
+  );
 }
