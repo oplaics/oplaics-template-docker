dba28f08eae4e0b3de1b9c28a26f722ea887fb1e
Recker <joseortiz112001@gmail.com>
2026-04-13 16:55:09 -0400

    feat: eliminar atributos innecesarios de la respuesta en las pruebas de creación de cuenta

--- backend/tests/Feature/Auth/AuthController/PostTest.php
+++ backend/tests/Feature/Auth/AuthController/PostTest.php
@@ -31,8 +31,6 @@ test('[POST] usuario puede crear una cuenta', function () {
                 'id',
                 'name',
                 'email',
-                'created_at',
-                'updated_at',
             ],
         ]);
 
