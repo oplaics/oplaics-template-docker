9c31e6fabf8ec1477eb62cb09046a361b3d49c55
Recker <joseortiz112001@gmail.com>
2026-04-13 09:53:46 -0400

    feat: habilitar rutas de autenticación 2FA y relogin

--- backend/routes/app/auth/auth.api.php
+++ backend/routes/app/auth/auth.api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\Auth\AuthController;
 Route::post('/login', [AuthController::class, 'login'])
   ->name('auth.login');
 
-// Route::post('/2fa', [AuthController::class, 'loginWith2FA'])
-//   ->middleware('auth:sanctum')
-//   ->name('auth.2fa');
+Route::post('/2fa', [AuthController::class, 'loginWith2FA'])
+  ->middleware('auth:sanctum')
+  ->name('auth.2fa');
 
-// Route::post('/2fa/resend', [AuthController::class, 'resendEmail'])
-//   ->middleware('auth:sanctum')
-//   ->name('auth.2fa.resend');
+Route::post('/2fa/resend', [AuthController::class, 'resendEmail'])
+  ->middleware('auth:sanctum')
+  ->name('auth.2fa.resend');
 
-// Route::post('/relogin', [AuthController::class, 'relogin'])
-//   ->middleware('auth:sanctum')
-//   ->name('auth.relogin');
+Route::post('/relogin', [AuthController::class, 'relogin'])
+  ->middleware('auth:sanctum')
+  ->name('auth.relogin');
 
 // Route::get('/logout', [AuthController::class, 'logout'])
 //   ->middleware('auth:sanctum')
