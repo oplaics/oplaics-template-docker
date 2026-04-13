7622b1bddf8a595aa44738c020f5bc5310700fda
Recker <joseortiz112001@gmail.com>
2026-04-13 09:53:54 -0400

    feat: agregar pruebas para autenticación de dos factores y relogin

--- backend/tests/Feature/Auth/AuthController/PostTest.php
+++ backend/tests/Feature/Auth/AuthController/PostTest.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Mail;
 
 uses(RefreshDatabase::class);
 
-test('Login via credenciales', function () {
+test('[POST] usuario puede logearse', function () {
     // $this->withoutExceptionHandling();
     $user = User::factory()->create([
         'password' => 'testing',
@@ -41,4 +41,145 @@ test('Login via credenciales', function () {
         ]);
 
     Mail::assertQueued(TfaCodeMail::class, 1);
+});
+
+test('[POST] usuario puede reenviar código de verificación por correo electrónico', function () {
+    Mail::fake();
+
+    $user = User::factory()->create();
+    Sanctum::actingAs(
+        $user,
+        ['2fa_required']
+    );
+
+    $response = $this->postJson(route('auth.2fa.resend'), [
+        'email' => $user->email,
+    ]);
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+        ]);
+
+    Mail::assertQueued(TfaCodeMail::class, 1);
+});
+
+test('[POST] usuario no puede reenviar código de verificación por correo electrónico si ocurre un error', function () {
+    Mail::fake();
+
+    $user = User::factory()->create();
+    Sanctum::actingAs(
+        $user,
+        ['2fa_required']
+    );
+
+    // Simular un error al enviar el correo
+    Mail::shouldReceive('to->queue')
+        ->once()
+        ->andThrow(new \Exception('Error al enviar el correo'));
+
+    $response = $this->postJson(route('auth.2fa.resend'), [
+        'email' => $user->email,
+    ]);
+
+    $response->assertStatus(500)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+            'error',
+        ]);
+});
+
+test('[POST] usuario puede verificar el código de doble factor', function () {
+    $securityCode = SecurityCode::factory()->create([
+        'type' => 'two_factor_auth',
+    ]);
+    $user = $securityCode->user;
+
+    Sanctum::actingAs(
+        $user,
+        ['2fa_required', 'remember_me']
+    );
+
+    $response = $this->postJson(route('auth.2fa'), [
+        'otp' => $securityCode->token,
+    ]);
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'apiKey',
+            'token_can',
+            'msg',
+            'user',
+            'permissions',
+            'roles',
+            'unreads',
+        ]);
+});
+
+test('[POST] usuario no puede verificar el código de doble factor con un código incorrecto', function () {
+    $securityCode = SecurityCode::factory()->create([
+        'type' => 'two_factor_auth',
+    ]);
+    $user = $securityCode->user;
+
+    Sanctum::actingAs(
+        $user,
+        ['2fa_required', 'remember_me']
+    );
+
+    $response = $this->postJson(route('auth.2fa'), [
+        'otp' => '123456', // Código incorrecto
+    ]);
+
+    $response->assertStatus(400)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+        ]);
+});
+
+test('[POST] usuario no puede verificar el código de doble factor con un código expirado', function () {
+    $securityCode = SecurityCode::factory()->create([
+        'type' => 'two_factor_auth',
+        'expire_at' => now()->subMinutes(1), // Código expirado
+    ]);
+    $user = $securityCode->user;
+
+    Sanctum::actingAs(
+        $user,
+        ['2fa_required', 'remember_me']
+    );
+
+    $response = $this->postJson(route('auth.2fa'), [
+        'otp' => $securityCode->token,
+    ]);
+
+    $response->assertStatus(400)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+        ]);
+});
+
+test('[POST] usuario puede hacer relogin con token valido', function () {
+    Sanctum::actingAs(
+        User::factory()->create(),
+        ['2fa_required']
+    );
+
+    $response = $this->postJson(route('auth.relogin'));
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'msg',
+            'user',
+            'token_can',
+            'permissions',
+            'roles',
+            'unreads',
+        ]);
 });
