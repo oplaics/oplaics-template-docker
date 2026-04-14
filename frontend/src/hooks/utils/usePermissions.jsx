7334dae8fbd69e3a21c74d7743db0642832c6b3c
Recker <joseortiz112001@gmail.com>
2026-04-14 15:12:03 -0400

    feat: actualizar selectores de roles y permisos para usar el estado de sesión

--- frontend/src/hooks/utils/usePermissions.jsx
+++ frontend/src/hooks/utils/usePermissions.jsx
@@ -4,14 +4,14 @@
 import { useSelector } from "react-redux";
 
 export function useRole(rolesToCheck) {
-  const roles = useSelector(state => state.auth.roles);
+  const roles = useSelector(state => state.session.roles);
   const hasRole = rolesToCheck?.some(role => roles.includes(role));
 
   return hasRole || false;
 }
 
 export function usePermission(perm) {
-  const permission = useSelector(state => state.auth.permissions[perm]);
+  const permission = useSelector(state => state.session.permissions[perm]);
 
   return permission || false;
 }
