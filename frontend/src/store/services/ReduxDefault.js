96f82a09cceca85636bd6c29921a4eea7bfee2c8
Recker <joseortiz112001@gmail.com>
2026-04-14 16:59:09 -0400

    feat: agregar baseQuery y manejo de errores en ReduxDefault

--- frontend/src/store/services/ReduxDefault.js
+++ frontend/src/store/services/ReduxDefault.js
@@ -5,7 +5,7 @@ export const baseQuery = fetchBaseQuery({
   baseUrl: `${window.location.protocol}//${window.location.hostname}/api/v1`,
   headers: { accept: "application/json" },
   prepareHeaders: (headers, { getState }) => {
-    const apiKey = getState().auth.apiKey;
+    const apiKey = getState().session.apiKey;
     if (apiKey) {
       headers.set("Authorization", `Bearer ${apiKey}`);
     }
@@ -13,29 +13,10 @@ export const baseQuery = fetchBaseQuery({
   },
 });
 
-export const baseQueryWithErrorHandling = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  if (result.error) {
-    const { error } = result;
-    if (error.status === 'FETCH_ERROR') {
-      api.dispatch(updateNotifer({ status: "offline" }));
-    } else {
-      api.dispatch(updateNotifer({
-        status: error.status,
-        text: error.data?.msg,
-      }));
-    }
-  }
-  return result;
-};
-
-/**
- * Default error handler for queries (for components using react-hook-form setError)
- */
 export const handleQueryError = (err, dispatch, setError) => {
   const { error } = err;
   if (error.status === 'FETCH_ERROR' || !error) {
-    dispatch(updateNotistack({ status: "offline" }));
+    dispatch(updateNotifer({ status: "offline" }));
   } else {
     if (error.status === 422 && error.data.errors && setError) {
       Object.keys(error.data.errors).forEach((key) => {
@@ -46,6 +27,6 @@ export const handleQueryError = (err, dispatch, setError) => {
       });
     }
 
-    dispatch(updateNotistack({ status: error.status, text: error.data.msg }));
+    dispatch(updateNotifer({ status: error.status, text: error.data.msg }));
   }
 };
