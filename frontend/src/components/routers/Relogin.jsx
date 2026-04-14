96b229918d612092da9e014cf7cbfaf2c789a3d5
Recker <joseortiz112001@gmail.com>
2026-04-14 17:41:49 -0400

    feat: eliminar propiedad notSave en la actualización de sesión en Relogin

--- frontend/src/components/routers/Relogin.jsx
+++ frontend/src/components/routers/Relogin.jsx
@@ -8,6 +8,7 @@ import Fallback from "./Fallback";
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import { updateSession } from "../../store/reducers/app/sessionReducer";
+import { useReloginMutation } from "../../store/services/auth/AuthApi";
 
 export default function Relogin({ children }) {
   const apiKey = useSelector((state) => state.session.apiKey);
@@ -16,7 +17,7 @@ export default function Relogin({ children }) {
   const [postRelogin, { isLoading }] = useReloginMutation();
 
   useEffect(() => {
-    dispatch(updateSession({ key: "relogin", value: false, notSave: true }));
+    dispatch(updateSession({ key: "relogin", value: false }));
     if (!apiKey || !relogin || isLoading) return;
     postRelogin();
   }, [apiKey, relogin, isLoading, postRelogin, dispatch]);
