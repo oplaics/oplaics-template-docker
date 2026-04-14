c73ce0b80327a9caaa93349dbd91441b536fc562
Recker <joseortiz112001@gmail.com>
2026-04-14 15:39:26 -0400

    feat: agregar componente ForgotPage para recuperación de contraseña

---
+++ frontend/src/pages/auth/ForgotPage.jsx
@@ -0,0 +1,12 @@
+import ContainerUI from "../../components/ui/ContainerUI";
+
+export default function ForgotPage() {
+  return (
+    <ContainerUI full>
+      Forgot Page
+
+      <br/>
+      <NavLink to="/auth">Ir a login</NavLink>
+    </ContainerUI>
+  )
+}
