7c1b38dedf35a010b8843bf493b24954f97b04bd
Recker <joseortiz112001@gmail.com>
2026-04-14 17:31:07 -0400

    feat: mejorar la página de inicio de sesión con formulario y validación

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,15 +1,102 @@
+/**
+ * React
+ */
 import { NavLink } from "react-router-dom";
+
+/**
+ * MUI
+ */
+import { Button, Grid } from "@mui/material";
+
+/**
+ * RHF
+ */
+import { FormProvider, useForm } from "react-hook-form";
+
+/**
+ * Components
+ */
 import ContainerUI from "../../components/ui/ContainerUI";
+import { InputHook } from "../../components/forms/Inputs/InputHook";
+import { InputPasswordHook } from "../../components/forms/Inputs/InputPasswordHook";
+import { CheckBoxHook } from "../../components/forms/checkbox/CheckboxHook";
+import { useLoginMutation } from "../../store/services/auth/AuthApi";
+import useNotifier from "../../hooks/utils/useNotifier";
 
 export default function LoginPage() {
+  useNotifier();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const methods = useForm({
+    defaultValues: {
+      email: "testing@test.test",
+      password: "password",
+    },
+  });
+
+  const onSubmit = (data) => {
+    login({
+      body: data
+    });
+  };
+
   return (
     <ContainerUI full>
-      LoginPage
+      <h1>Login</h1>
+      <FormProvider {...methods}>
+        <Grid
+          container
+          component="form"
+          onSubmit={methods.handleSubmit(onSubmit)}
+          autoComplete="off"
+          spacing={2}
+        >
+          <Grid size={{ xs: 12, md: 6 }}>
+            <InputHook
+              name="email"
+              label="Correo electrónico"
+              disabled={isLoading}
+              fullWidth
+              rules={{
+                required: "El correo electrónico es requerido",
+              }}
+            />
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <InputPasswordHook
+              name="password"
+              label="Contraseña"
+              disabled={isLoading}
+              fullWidth
+              rules={{
+                required: "La contraseña es requerida",
+                minLength: {
+                  value: 8,
+                  message: "La contraseña debe tener al menos 6 caracteres",
+                },
+              }}
+            />
+          </Grid>
+          <Grid size={12}>
+            <CheckBoxHook
+              name="remember"
+              label="Recordar sesión"
+              disabled={isLoading}
+            />
+          </Grid>
+          <Grid size={12}>
+            <Button loading={isLoading} type="submit" variant="contained" color="primary">
+              Iniciar sesión
+            </Button>
+          </Grid>
+        </Grid>
+      </FormProvider>
 
-      <br/>
+      <br />
       <NavLink to="/auth/register">Ir a Register</NavLink>
-      <br/>
+      <br />
       <NavLink to="/auth/forgot">Reiniciar contraseña</NavLink>
     </ContainerUI>
-  )
+  );
 }
