6808d2ede44290fea5c42694d9aeec681c0f11d0
Recker <joseortiz112001@gmail.com>
2026-04-14 17:41:36 -0400

    feat: envolver Outlet en el componente Relogin dentro de AuthLayoutWrapper

--- frontend/src/components/routers/AuthLayout.jsx
+++ frontend/src/components/routers/AuthLayout.jsx
@@ -13,9 +13,14 @@ import { SnackbarProvider } from "notistack";
  */
 import { Provider } from "react-redux";
 import store from "../../store";
+import Relogin from "./Relogin";
 
 function AuthLayoutWrapper() {
-  return <Outlet />;
+  return (
+    <Relogin>
+      <Outlet />
+    </Relogin>
+  );
 }
 
 export default function AuthLayout() {
