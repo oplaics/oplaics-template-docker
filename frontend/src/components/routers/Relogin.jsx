1a787ae19d9b4f7b64594bd1e52b73d121cd6bf1
Recker <joseortiz112001@gmail.com>
2026-04-14 15:16:15 -0400

    feat: agregar componente Relogin para manejo de sesión y carga de estado

---
+++ frontend/src/components/routers/Relogin.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+/**
+ * Components
+ */
+import Fallback from "./Fallback";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { updateSession } from "../../store/reducers/app/sessionReducer";
+
+export default function Relogin({ children }) {
+  const apiKey = useSelector((state) => state.session.apiKey);
+  const relogin = useSelector((state) => state.session.relogin);
+  const dispatch = useDispatch();
+  const [postRelogin, { isLoading }] = useReloginMutation();
+
+  useEffect(() => {
+    dispatch(updateSession({ key: "relogin", value: false, notSave: true }));
+    if (!apiKey || !relogin || isLoading) return;
+    postRelogin();
+  }, [apiKey, relogin, isLoading, postRelogin, dispatch]);
+
+  if (relogin || isLoading)
+    return (
+      <Fallback
+        text="Estamos cargando algunas cosas, por favor espere..."
+        loading
+      />
+    );
+  return children;
+}
