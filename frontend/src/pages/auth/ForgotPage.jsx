d75aa1b2b4e21b7d1e620c629fff67e275eea61a
Recker <joseortiz112001@gmail.com>
2026-04-15 16:16:28 -0400

    feat: mejorar página de recuperación de contraseña con formularios para OTP y nueva contraseña

--- frontend/src/pages/auth/ForgotPage.jsx
+++ frontend/src/pages/auth/ForgotPage.jsx
@@ -1,13 +1,84 @@
+/**
+ * React
+ */
 import { NavLink } from "react-router-dom";
+
+/**
+ * RHF
+ */
+import { FormProvider, useForm } from "react-hook-form";
+
+/**
+ * MUI
+*/
+import { Button, Grid } from "@mui/material";
+
+/**
+ * Components
+*/
+import useTitleHook from "../../hooks/utils/useTitleHook";
 import ContainerUI from "../../components/ui/ContainerUI";
+import { InputHook } from "../../components/forms/Inputs/InputHook";
+import { InputPasswordHook } from "../../components/forms/Inputs/InputPasswordHook";
 
 export default function ForgotPage() {
+  useTitleHook('2FA - App');
+  const form1 = useForm();
+  const form2 = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <ContainerUI full>
-      Forgot Page
+      <h1>2FA Page</h1>
+      
+      <FormProvider {...form1}>
+        <Grid container component="form" onSubmit={form1.handleSubmit(onSubmit)} autoComplete="off" spacing={2}>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <InputHook
+              name="otp"
+              label="Código de autenticación"
+              fullWidth
+            />
+          </Grid>
+          <Grid size={12}>
+            <Button type="submit" variant="contained" fullWidth>
+              Verificar
+            </Button>
+          </Grid>
+        </Grid>
+      </FormProvider>
 
-      <br/>
-      <NavLink to="/auth">Ir a login</NavLink>
+      <br />
+
+      <FormProvider {...form2}>
+        <Grid container component="form" onSubmit={form2.handleSubmit(onSubmit)} autoComplete="off" spacing={2}>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <InputPasswordHook
+              name="new_password"
+              label="Nueva contraseña"
+              fullWidth
+            />
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <InputPasswordHook
+              name="new_password_confirmation"
+              label="Confirmar contraseña"
+              fullWidth
+            />
+          </Grid>
+          <Grid size={12}>
+            <Button type="submit" variant="contained" fullWidth>
+              Cambiar contraseña
+            </Button>
+          </Grid>
+        </Grid>
+      </FormProvider>
+
+      <br />
+      <NavLink to="/auth/logout">Cerrar sección</NavLink>
     </ContainerUI>
   )
 }
