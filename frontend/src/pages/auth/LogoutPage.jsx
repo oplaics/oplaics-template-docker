f306f0289f373e555801218a67b17d24c12969c8
Recker <joseortiz112001@gmail.com>
2026-04-15 14:41:15 -0400

    feat: implementar página de cierre de sesión con mensaje de espera

--- frontend/src/pages/auth/LogoutPage.jsx
+++ frontend/src/pages/auth/LogoutPage.jsx
@@ -1,5 +1,30 @@
+/**
+ * React
+ */
+import { useEffect } from "react"
+import { useSearchParams } from "react-router-dom";
+
+/**
+ * Components
+ */
+import Fallback from "../../components/routers/Fallback";
+import useTitleHook from "../../hooks/utils/useTitleHook";
+
+/**
+ * Redux
+ */
+import { useLogoutMutation } from "../../store/services/auth/AuthApi";
+
 export default function LogoutPage() {
+  useTitleHook('Logout - App');
+  const [searchParams] = useSearchParams();  
+  const [getLogout] = useLogoutMutation();
+
+  useEffect(() => {
+    getLogout({mode: searchParams.get('mode')});
+  }, [getLogout, searchParams]);
+
   return (
-    <div>LogoutPage</div>
+    <Fallback text='Estamos cerrando su sesion, espere por favor...' />
   )
 }
